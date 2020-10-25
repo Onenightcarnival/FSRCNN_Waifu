71c5383deb37a61fb0434621cd8e2481e3f648ad
Chao Li <244183238li@gmail.com>
2020-10-24 22:23:31 -0400

    modify image layout

--- app.py
+++ app.py
@@ -22,17 +22,16 @@ def get_image_download_link(img):
 	return href
 
 st.title("FSRCNN Anime Image 2X Scale Up")
+col1, col2 = st.beta_columns(2)
 model = keras.models.load_model('FSRCNN_Waifu.h5')
 uploaded_file = st.file_uploader("Choose an image", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
     image = np.array(image)
     image = image[:, :, ::-1].copy()
-    st.image(image, caption='Uploaded Img.', use_column_width=True, channels='BGR')
-    st.write("")
-    st.write("Upscaling...")
+    col1.image(image, caption='Uploaded Img.', use_column_width=True, channels='BGR')
     img = upscale(image, model)
-    st.image(img, caption='Scaled up Img.', use_column_width=True, channels='BGR')
+    col2.image(img, caption='Scaled up Img.', use_column_width=True, channels='BGR')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = Image.fromarray(img)
     st.markdown(get_image_download_link(img), unsafe_allow_html=True)
